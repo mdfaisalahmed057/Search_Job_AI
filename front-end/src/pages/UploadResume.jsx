e702d4c00be0c1cb54f1918abce970ec66b33a82
mdfaisalahmed057 <mdfais2995@gmail.com>
2026-06-30 23:18:55 +0530

    fix: correct API endpoint for job search request

--- front-end/src/pages/UploadResume.jsx
+++ front-end/src/pages/UploadResume.jsx
@@ -42,7 +42,7 @@ function UploadResume() {
       formData.append('file', file); 
     
       
-       const response = await fetch('https://backend-for-job-scrap.onrender.com/api/jobs/extract_resume', {
+       const response = await fetch('https://backend-for-job-scrap.onrender.com/extract_resume', {
         method: 'POST',
         body: formData,
       });
